36c59ad62b9cd713069647c9aa8125716fcb0e5e
Shakil <shakil2995@gmail.com>
2022-04-04 01:56:56 +0600

    updated

--- header.php
+++ header.php
@@ -11,8 +11,12 @@
     <title>Contact us</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-md  mb-4">
+<!-- <nav class="navbar navbar-expand-md  mb-4">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
       <a class="navbar-brand" href="index.php">FORM</a>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
@@ -20,6 +24,24 @@
           <li class="nav-item">
             <a class="nav-link" href="admin.php">Admin</a>
           </li>
-
         </ul>
-    </nav>
+      </div>
+    </nav> -->
+
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">FORM</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+      <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="admin.php">Admin</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- <br><br><br> -->
