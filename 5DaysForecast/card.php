b22810c6c67654c109b5e300f292991cefaea10c
Shakil <shakil2995@gmail.com>
2022-04-07 19:08:36 +0600

    reformatted , added core functionality

--- 5DaysForecast/card.php
+++ 5DaysForecast/card.php
@@ -1,80 +1,37 @@
-<?php 
-$main = $decoded->list[0]->weather[0]->main;
-$temp = $decoded->list[0]->main->temp;
-$temp_min = $decoded->list[0]->main->temp_min;
-$temp_max = $decoded->list[0]->main->temp_max;
-$temp_feelsLike = $decoded->list[0]->main->feels_like;
-$icon = $decoded->list[0]->weather[0]->icon;
-$epoch=$decoded->list[0]->dt;
-$time=new DateTime("@$epoch");
-?>
 <div class="main-contaner"> 
 <form class="search" action="index.php" method="get">
     <div class="center">
         <input type="text" style="width:85%" name="city_name" placeholder="Enter City Name">
         <button class="btn btn-primary btn-md border border-radius center" type="submit">GO</button>
     </div>
-</form>
+</form>    
 <div class=" card-group w-80 ">
-  <div class="card c-card ">
-    <img class="card-img-top" style="width=30px"; src="https://openweathermap.org/img/wn/<?php echo $icon ?>@2x.png" alt="Card image cap">
-    <div class="card-body">
-    <p class="card-text time"><?php echo $time->format('d/m/D H:i:s') ?></p>
-      <h5 class="card-title">Condition : <?php echo $main ?> </h5>
-      <p class="card-text">Current Tempereture : <?php echo $temp ?>°</p>
-      <p class="card-text">Minimum Tempereture : <?php echo $temp_min ?>°</p>
-      <p class="card-text">Maximum Tempereture : <?php echo $temp_max ?>°</p>
-      <p class="card-text">Feels like : <?php echo $temp_feelsLike ?>°</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+        <?php for($i=0;$i<=39;$i+=8): 
+            $main = $decoded->list[$i]->weather[0]->main;
+            $temp = $decoded->list[$i]->main->temp;
+            $temp_min = $decoded->list[$i]->main->temp_min;
+            $temp_max = $decoded->list[$i]->main->temp_max;
+            $temp_feelsLike = $decoded->list[$i]->main->feels_like;
+            $icon = $decoded->list[$i]->weather[0]->icon;
+            $epoch=$decoded->list[$i]->dt;
+            $time=new DateTime("@$epoch");
+            // echo $i;
+            ?>
+            <div class="card c-card ">
+                <img class="card-img-top" style="width=30px"; src="https://openweathermap.org/img/wn/<?php echo $icon ?>@2x.png" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text time"><?php echo $time->format('d/m/D H:i:s') ?></p>
+                    <h5 class="card-title">Condition : <?php echo $main ?> </h5> 
+                    <br>
+                    <p class="card-text">Current Tempereture : <?php echo $temp ?>°</p>
+                    <p class="card-text">Min Tempereture : <?php echo $temp_min ?>°</p>
+                    <p class="card-text">Max Tempereture : <?php echo $temp_max ?>°</p>
+                    <p class="card-text">Feels like : <?php echo $temp_feelsLike ?>°</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+
+    <?php endfor; ?>
     </div>
-  </div>
-  <div class="card c-card ">
-    <img class="card-img-top" style="width=30px"; src="https://openweathermap.org/img/wn/<?php echo $icon ?>@2x.png" alt="Card image cap">
-    <div class="card-body">
-    <p class="card-text time"><?php echo $time->format('d/m/D H:i:s') ?></p>
-      <h5 class="card-title">Condition : <?php echo $main ?> </h5>
-      <p class="card-text">Current Tempereture : <?php echo $temp ?>°</p>
-      <p class="card-text">Minimum Tempereture : <?php echo $temp_min ?>°</p>
-      <p class="card-text">Maximum Tempereture : <?php echo $temp_max ?>°</p>
-      <p class="card-text">Feels like : <?php echo $temp_feelsLike ?>°</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card c-card ">
-    <img class="card-img-top" style="width=30px"; src="https://openweathermap.org/img/wn/<?php echo $icon ?>@2x.png" alt="Card image cap">
-    <div class="card-body">
-    <p class="card-text time"><?php echo $time->format('d/m/D H:i:s') ?></p>
-      <h5 class="card-title">Condition : <?php echo $main ?> </h5>
-      <p class="card-text">Current Tempereture : <?php echo $temp ?>°</p>
-      <p class="card-text">Minimum Tempereture : <?php echo $temp_min ?>°</p>
-      <p class="card-text">Maximum Tempereture : <?php echo $temp_max ?>°</p>
-      <p class="card-text">Feels like : <?php echo $temp_feelsLike ?>°</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card c-card ">
-    <img class="card-img-top" style="width=30px"; src="https://openweathermap.org/img/wn/<?php echo $icon ?>@2x.png" alt="Card image cap">
-    <div class="card-body">
-    <p class="card-text time"><?php echo $time->format('d/m/D H:i:s') ?></p>
-      <h5 class="card-title">Condition : <?php echo $main ?> </h5>
-      <p class="card-text">Current Tempereture : <?php echo $temp ?>°</p>
-      <p class="card-text">Minimum Tempereture : <?php echo $temp_min ?>°</p>
-      <p class="card-text">Maximum Tempereture : <?php echo $temp_max ?>°</p>
-      <p class="card-text">Feels like : <?php echo $temp_feelsLike ?>°</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card c-card ">
-    <img class="card-img-top" style="width=30px"; src="https://openweathermap.org/img/wn/<?php echo $icon ?>@2x.png" alt="Card image cap">
-    <div class="card-body">
-    <p class="card-text time"><?php echo $time->format('d/m/D H:i:s') ?></p>
-      <h5 class="card-title">Condition : <?php echo $main ?> </h5>
-      <p class="card-text">Current Tempereture : <?php echo $temp ?>°</p>
-      <p class="card-text">Minimum Tempereture : <?php echo $temp_min ?>°</p>
-      <p class="card-text">Maximum Tempereture : <?php echo $temp_max ?>°</p>
-      <p class="card-text">Feels like : <?php echo $temp_feelsLike ?>°</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  </div>
 </div>
--- 5DaysForecast/header.php
+++ 5DaysForecast/header.php
@@ -7,12 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="styles.css">
-    <title>5 Days Forecast</title>
+    <title>Weather Forecast</title>
 </head>
 <body>
 
     <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">5Days Forecast</a>
+    <a class="navbar-brand" href="index.php"><span style="color:aquamarine">Shakil's </span>Forecast</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
