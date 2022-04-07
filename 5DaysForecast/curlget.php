cb298fb7a5c21af5587a8bb329faca9b9231b5de
Shakil <shakil2995@gmail.com>
2022-04-07 13:50:41 +0600

    connected api

--- 5DaysForecast/curlget.php
+++ 5DaysForecast/curlget.php
@@ -1,8 +1,15 @@
 <?php 
 $api_key="398e310b24f291b753fabdb60b31cc14";
-// echo $api_key;
+$url = "http://api.openweathermap.org/data/2.5/weather?q=dhaka&units=metric&appid=$api_key";
 $ch = curl_init();
-$url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=$api_key";
-echo $url;
-
+// echo $url;
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$res = curl_exec($ch);
+if($err = curl_error($ch)){
+    echo $err;
+} else {
+    $decoded = json_decode($res);
+    print_r($decoded);
+}
 ?>
